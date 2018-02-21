8c40926398d2abd245cbd1ecbbabe5c9592b208d
Anton Hansson <hansson@google.com>
2018-02-21 06:36:50 +0000

    ham: devicesettings: Set LOCAL_SDK_VERSION to current
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Bug: 73535841
    Change-Id: Id5b7526dee882958ee6562cda19ef702b26144f9

--- devicesettings/Android.mk
+++ devicesettings/Android.mk
@@ -5,6 +5,8 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
 LOCAL_PACKAGE_NAME := DeviceSettings
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
