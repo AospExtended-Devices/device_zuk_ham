b88d3e9ccbd6cd041602bbd1e22373bef0f88fab
Wang Han <416810799@qq.com>
2019-10-11 14:40:25 +0800

    ham: Remove more deprecated display flags
    Change-Id: I7924f76ed864dea9cefbe2585be629bd2239c447
    
    # Conflicts:
    #	BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,16 +125,6 @@ NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
 
-# Shader cache config options
-# Maximum size of the  GLES Shaders that can be cached for reuse.
-# Increase the size if shaders of size greater than 12KB are used.
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-
-# Maximum GLES shader cache size for each app to store the compiled shader
-# binaries. Decrease the size if RAM or Flash Storage size is a limitation
-# of the device.
-MAX_EGL_CACHE_SIZE := 2048*1024
-
 # Exclude serif fonts for saving system.img size.
 EXCLUDE_SERIF_FONTS 	:= true
 
